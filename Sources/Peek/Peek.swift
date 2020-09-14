05d9da70a7bf328cffc8fc5e482a59339fd3a0cb
milos <milos@screensailor.net>
2020-09-14 03:53:27 +0100

    [trunk] park

--- Sources/Peek/Peek.swift
+++ Sources/Peek/Peek.swift
@@ -38,3 +38,21 @@ extension Peek: CustomStringConvertible, CustomDebugStringConvertible {
     @inlinable public var description: String { log() }
     @inlinable public var debugDescription: String { log() }
 }
+
+extension Peek {
+
+    public struct Error: Swift.Error, Hashable, CustomStringConvertible {
+        public let message: String
+        public let function: String
+        public let file: String
+        public let line: Int
+        public var location: CodeLocation { .init(function: function, file: file, line: line) }
+        public var description: String { "⚠️ \(message) \(location)" }
+        public init(_ message: String, _ function: String = #function,  _ file: String = #file, _ line: Int = #line) {
+            self.message = message
+            self.function = function
+            self.file = file
+            self.line = line
+        }
+    }
+}
--- Sources/Peek/Striing.swift
+++ Sources/Peek/Striing.swift
@@ -8,21 +8,3 @@ extension String {
         Peek.Error(self, function, file, line)
     }
 }
-
-extension Peek {
-
-    public struct Error: Swift.Error, Hashable, CustomStringConvertible {
-        public let message: String
-        public let function: String
-        public let file: String
-        public let line: Int
-        public var location: CodeLocation { .init(function: function, file: file, line: line) }
-        public var description: String { "\(message) \(location)" }
-        public init(_ message: String, _ function: String = #function,  _ file: String = #file, _ line: Int = #line) {
-            self.message = message
-            self.function = function
-            self.file = file
-            self.line = line
-        }
-    }
-}
