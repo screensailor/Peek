7bfa5ed1a266c1252016deaf2ca1ea44a4b779e3
milos <milos@screensailor.net>
2020-12-21 09:16:56 +0000

    park

--- Sources/Peek/Optional.swift
+++ Sources/Peek/Optional.swift
@@ -1,9 +1,62 @@
-extension Optional: CustomStringConvertible {
+extension Optional { // avoiding CustomStringConvertible conformance
     
-    public var description: String {
-        switch self {
-        case .none: return "\(Wrapped.self)?.none"
-        case .some(let o): return String(describing: o)
-        }
+    @inlinable
+    @discardableResult
+    public func peek(
+        as level: OSLogType = .debug,
+        function: String = #function,
+        file: String = #file,
+        line: Int = #line
+    ) -> Self {
+        logger.log(level: level, "\(String(describing: self)) \(here(function, file, line))")
+        return self
+    }
+    
+    @inlinable
+    @discardableResult
+    public func peek<Message>(
+        _ message: @escaping @autoclosure () -> Message,
+        as level: OSLogType = .debug,
+        function: String = #function,
+        file: String = #file,
+        line: Int = #line
+    ) -> Self
+    where Message: CustomStringConvertible
+    {
+        logger.log(level: level, "\(message()) \(String(describing: self)) \(here(function, file, line))")
+        return self
+    }
+
+    @inlinable
+    @discardableResult
+    public func peek<Property>(
+        _ keyPath: KeyPath<Self, Property>,
+        as level: OSLogType = .debug,
+        function: String = #function,
+        file: String = #file,
+        line: Int = #line
+    ) -> Self
+    where Property: CustomStringConvertible
+    {
+        logger.log(level: level, "\(self[keyPath: keyPath]) \(here(function, file, line))")
+        return self
+    }
+
+    @inlinable
+    @discardableResult
+    public func peek<Message, Property>(
+        _ message: @escaping @autoclosure () -> Message,
+        _ keyPath: KeyPath<Self, Property>,
+        as level: OSLogType = .debug,
+        function: String = #function,
+        file: String = #file,
+        line: Int = #line
+    ) -> Self
+    where
+        Message: CustomStringConvertible,
+        Property: CustomStringConvertible
+    {
+        logger.log(level: level, "\(message()) \(self[keyPath: keyPath]) \(here(function, file, line))")
+        return self
     }
 }
