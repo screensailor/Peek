0f3692e56058305061767de6c2a8eb364d0f1277
milos <milos@screensailor.net>
2021-05-07 16:27:20 +0100

    park

--- Sources/Peek/Event.swift
+++ Sources/Peek/Event.swift
@@ -11,7 +11,7 @@ public protocol State {
 
 public struct Context: Identifiable, Hashable, CustomStringConvertible {
     
-    public var id: String { description }
+    public let id: UUID = .init()
     
     public let name: String
     public let location: CodeLocation
@@ -32,9 +32,9 @@ public struct Context: Identifiable, Hashable, CustomStringConvertible {
 }
 
 extension Context {
-    public static let states = PassthroughSubject<(state: State, context: Context), Never>()
-    public static let events = PassthroughSubject<(event: Event, state: State, context: Context), Never>()
-    public static let errors = PassthroughSubject<(event: Event, state: State, context: Context, error: Error), Never>()
+    public static let states = PassthroughSubject<State, Never>()
+    public static let events = PassthroughSubject<(event: Event, state: State), Never>()
+    public static let errors = PassthroughSubject<(error: Error, event: Event, state: State), Never>()
 }
 
 extension Context {
@@ -59,14 +59,19 @@ private extension Context {
 extension Event {
     
     @discardableResult
-    public func `in`(_ context: Context) -> Self { // TODO: capture code location info
+    public func `in`(
+        _ context: Context,
+        _ function: String = #function,
+        _ file: String = #file,
+        _ line: Int = #line
+    ) -> Self { // TODO: capture code location info
         Thread.onMainThread {
             let state = context.state
             do {
                 try state.on(self)
-                Context.events.send((self, state, context))
+                Context.events.send((self, state))
             } catch {
-                Context.errors.send((self, state, context, error))
+                Context.errors.send((error, self, state))
             }
         }
         return self
@@ -87,14 +92,14 @@ public struct Enter<This: State>: Event {
     
     @discardableResult
     public func `in`(_ context: Context) -> This { // TODO: capture code location info
-        let state = This.init(context: context)
+        let state = This(context: context)
         Thread.onMainThread {
             let old = Context.queue.sync { Context.state[context] }
             old?.willExit()
             Context.queue.sync {
                 Context.state[context] = state
             }
-            Context.states.send((state, context))
+            Context.states.send(state)
             (self as Event).in(context)
             old?.didExit()
         }
@@ -137,47 +142,47 @@ private extension Thread {
 
 extension PassthroughSubject
 where
-    Output == (state: State, context: Context),
+    Output == State,
     Failure == Never
 {
     
-    public subscript<S: State>(_: S.Type) -> AnyPublisher<(state: S, context: Context), Never> {
-        compactMap{ o in (o.state as? S).map{ s in (s, o.context) } }.eraseToAnyPublisher()
+    public subscript<S: State>(_: S.Type) -> AnyPublisher<S, Never> {
+        compactMap{ o in (o as? S) }.eraseToAnyPublisher()
     }
 }
 
 extension PassthroughSubject
 where
-    Output == (event: Event, state: State, context: Context),
+    Output == (event: Event, state: State),
     Failure == Never
 {
     
-    public subscript<E: Event>(_: E.Type) -> AnyPublisher<(event: E, state: State, context: Context), Never> {
-        compactMap{ o in (o.event as? E).map{ e in (e, o.state, o.context) } }.eraseToAnyPublisher()
+    public subscript<E: Event>(_: E.Type) -> AnyPublisher<(event: E, state: State), Never> {
+        compactMap{ o in (o.event as? E).map{ e in (e, o.state) } }.eraseToAnyPublisher()
     }
     
-    public subscript<E: Event, S: State>(_: E.Type, _: S.Type) -> AnyPublisher<(event: E, state: S, context: Context), Never> {
+    public subscript<E: Event, S: State>(_: E.Type, _: S.Type) -> AnyPublisher<(event: E, state: S), Never> {
         compactMap{ o in
             guard let e = o.event as? E, let s = o.state as? S else { return nil }
-            return (e, s, o.context)
+            return (e, s)
         }.eraseToAnyPublisher()
     }
 }
 
 extension PassthroughSubject
 where
-    Output == (event: Event, state: State, context: Context, error: Error),
+    Output == (error: Error, event: Event, state: State),
     Failure == Never
 {
     
-    public subscript<E: Event>(_: E.Type) -> AnyPublisher<(event: E, state: State, context: Context, error: Error), Never> {
-        compactMap{ o in (o.event as? E).map{ e in (e, o.state, o.context, o.error) } }.eraseToAnyPublisher()
+    public subscript<E: Event>(_: E.Type) -> AnyPublisher<(error: Error, event: E, state: State), Never> {
+        compactMap{ o in (o.event as? E).map{ e in (o.error, e, o.state) } }.eraseToAnyPublisher()
     }
     
-    public subscript<E: Event, S: State>(_: E.Type, _: S.Type) -> AnyPublisher<(event: E, state: S, context: Context, error: Error), Never> {
+    public subscript<E: Event, S: State>(_: E.Type, _: S.Type) -> AnyPublisher<(error: Error, event: E, state: S), Never> {
         compactMap{ o in
             guard let e = o.event as? E, let s = o.state as? S else { return nil }
-            return (e, s, o.context, o.error)
+            return (o.error, e, s)
         }.eraseToAnyPublisher()
     }
 }
